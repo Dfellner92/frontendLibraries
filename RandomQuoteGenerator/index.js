cd889df3a929bad86e023a09264da9b1a079a8f0
Dan Fellner <dfellner92@gmail.com>
2021-11-06 20:52:33 -0400

    now adding react logic to random quote machine

--- RandomQuoteGenerator/index.js
+++ RandomQuoteGenerator/index.js
@@ -1,15 +1,29 @@
 class Card extends React.Component {
-  constructor(props) {
-    super(props);
+  
+    constructor(props) {
+      super(props)
+    }
+    
+     
+    componentWillMount() {
+      
+    }
+    
+      
+    render() {
+      return <div >
+        <wrapper id="quote-box">
+          <h1>Random Quote Generator</h1>
+          <div id="text"></div>
+          <div id="author"></div>
+          <div id="new-quote"></div>
+          <a id="tweet-quote" href="https://www.twitter.com"></a>
+        </wrapper>
+        
+      </div>;
+      }
+    
   }
-
-  render() {
-    return (
-      <div>
-        <p>I was built with a Class component extending React.Component.</p>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Card />, document.getElementById("root"));
+  
+  
+  ReactDOM.render(<Card />, document.getElementById("root"));
