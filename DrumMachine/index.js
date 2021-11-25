565c5f19d204e409a90628915aee2734c40030e1
Dan Fellner <dfellner92@gmail.com>
2021-11-25 15:44:40 -0500

    drum machine restart

--- DrumMachine/index.js
+++ DrumMachine/index.js
@@ -2,87 +2,29 @@
 /* eslint-disable react/prop-types, react/no-multi-comp */
 
 // eslint-disable-next-line no-unused-vars
-const projectName = "drum-machine";
+const projectName = 'drum-machine';
 
-const bankOne = [
-  {
-    keyCode: 81,
-    keyTrigger: "Q",
-    id: "Heater-1",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-  },
-  {
-    keyCode: 87,
-    keyTrigger: "W",
-    id: "Heater-2",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-  },
-  {
-    keyCode: 69,
-    keyTrigger: "E",
-    id: "Heater-3",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-  },
-  {
-    keyCode: 65,
-    keyTrigger: "A",
-    id: "Heater-4",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-  },
-  {
-    keyCode: 83,
-    keyTrigger: "S",
-    id: "Clap",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-  },
-  {
-    keyCode: 68,
-    keyTrigger: "D",
-    id: "Open-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-  },
-  {
-    keyCode: 90,
-    keyTrigger: "Z",
-    id: "Kick-n'-Hat",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-  },
-  {
-    keyCode: 88,
-    keyTrigger: "X",
-    id: "Kick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-  },
-  {
-    keyCode: 67,
-    keyTrigger: "C",
-    id: "Closed-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-  },
-];
-
-class DrumMachine extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      keys: ["Q", "W", "A", "E", "S", "D", "Z", "X", "C"]
+    };
   }
-
+ 
   render() {
+    const { keys } = this.state
+    
     return (
-      <div className="container" id="drum-machine">
-        <div id="display">Hello</div>
-        {bankOne.map((obj) => {
-          return (
-            <div id={obj.id} className="drum-pad">
-              {obj.keyTrigger}
-              <audio id={obj.keyTrigger} className="clip" src={obj.url}>
-                <source src={obj.url} type="audio/mp3" />
-              </audio>
-            </div>
-          );
-        })}
+      <div id="drum-machine">
+        <div id="display">{keys.map((key) => (
+            <div>{key}</div>
+          ))}</div>
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<DrumMachine />, document.getElementById("root"));
+ 
+
+ReactDOM.render(<App />, document.getElementById('root'));
