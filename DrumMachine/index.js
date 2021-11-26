44757ed08b6325264f531130081c0366a9e673fc
Dan Fellner <dfellner92@gmail.com>
2021-11-25 21:27:59 -0500

    fixed box render

--- DrumMachine/index.js
+++ DrumMachine/index.js
@@ -2,35 +2,33 @@
 /* eslint-disable react/prop-types, react/no-multi-comp */
 
 // eslint-disable-next-line no-unused-vars
-const projectName = 'drum-machine';
+const projectName = "drum-machine";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      keys: ["Q", "W", "A", "E", "S", "D", "Z", "X", "C"]
+      keys: ["Q", "W", "A", "E", "S", "D", "Z", "X", "C"],
     };
   }
- 
+
   render() {
-    const { keys } = this.state
-    
+    const { keys } = this.state;
+
     return (
       <div id="drum-machine">
         <div id="display">
           {keys.map((key, index) => (
-            <Box text={key} key={index}/>
+            <Box text={key} key={index} />
           ))}
         </div>
       </div>
-    )
+    );
   }
 }
 
 const Box = (props) => {
-    <div className="box">
-        {props.text}
-    </div>
-}
+  return <div className="box">{props.text}</div>;
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
